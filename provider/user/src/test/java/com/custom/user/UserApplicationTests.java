1383da7550de1074d0b8597f4227599f1b828944
张彪 <2213733404@qq.com>
2020-12-16 15:37:38 +0800

    初始化内容

---
+++ provider/user/src/test/java/com/custom/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.custom.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
